1aede10c4f7d9c1e96c1dbbaedfb02d48831c704
LJS-91 <lucaschaumkessel@outlook.com>
2024-01-15 17:25:53 +0100

    backup exercises up to 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
 
+const array = arguments[0];
+let newArray = array;
+    for (i=1; i < arguments.length; i++)
+    {
+        for (j = 0; j < array.length; j++) {
+            if (newArray[j] === arguments[i]) {
+                newArray = newArray.toSpliced(j,1);
+            }
+        }
+    }
+return newArray;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+// Solution 1:    
+/*if ((year%4 == 0 && year%100 != 0) || (year%100 == 0 && year%400 == 0)){
+    return true;
+}
+else {
+    return false;
+}*/
+
+//Solution 2:
+return year%4 == 0 && (year%100 != 0 || year%400 == 0);
 };
 
 // Do not edit below this line
